cfb498249c89b9dc62bd730d08952d20dde1ce2b
Mark Leybourne <mleyb@users.noreply.github.com>
2018-04-12 17:52:48 +0100

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace inspirobot
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(async (context) =>
+            {
+                string inspirobotUrl = "http://inspirobot.me/api?generate=true";
+    
+                string resultContent;
+
+                using(var client = new HttpClient())
+                {
+                    var result = await client.GetAsync(inspirobotUrl);
+
+                    resultContent = await result.Content.ReadAsStringAsync();
+                }
+
+                context.Response.Redirect(resultContent);
+            });
+        }
+    }
+}
